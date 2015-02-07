054cb0500fdc757eea82148cc0551967a6675259
Fabian Schmengler <fschmengler@sgh-it.eu>
2015-02-07 15:52:40 +0100

    Style checkmark in popup for selected translation.

--- js/sse/translationhints.js
+++ js/sse/translationhints.js
@@ -8,24 +8,28 @@ document.observe('dom:loaded',
         );
         var decorateTranslationHints = function(textNode) {
             var newText =  textNode.nodeValue.replace(/\[__(.*?)__\]\(\(__\((.*?)\):(.*?)(?:__\((.*?)\):(.*?))?(\(cached\))?\)\)/g,
-            	function(match, translated, code, codeSources, text, textSources, cached) {
-            		var result = translated + ' <span class="translation-hint-icon">i</span>';
-            		result += '<span class="translation-hint-popup">';
-            		result += '<b>'+ code +':</b><br/>';
-            		result += codeSources.replace(/\|/g, '<br/>');
-            		if (text) {
-                		result += '<b>'+ text +':</b><br/>';
-                		result += textSources.replace(/\|/g, '<br/>');
-            		}
-            		if (cached) {
-            			result += cached;
-            		}
-            		result += '</span>';
-            		return result;
-            	}
+                function(match, translated, code, codeSources, text, textSources, cached) {
+                    var result = translated + ' <span class="translation-hint-icon">i</span>';
+                    result += '<span class="translation-hint-popup">';
+                    result += '<b>'+ code +':</b><br/>';
+                    result += codeSources
+                        .replace(/\|/g, '<br/>')
+                        .replace(/\(x\)/g, '<span class="translation-hint-check">&#x2714;</span>');
+                    if (text) {
+                        result += '<b>'+ text +':</b><br/>';
+                        result += textSources
+                            .replace(/\|/g, '<br/>')
+                            .replace(/\(x\)/g, '<span class="translation-hint-check">&#x2714;</span>');
+                    }
+                    if (cached) {
+                        result += cached;
+                    }
+                    result += '</span>';
+                    return result;
+                }
             );
             if (newText == textNode.nodeValue) {
-            	return null;
+                return null;
             }
             var tmpElement = document.createElement('span');
             tmpElement.innerHTML = newText;
@@ -36,20 +40,20 @@ document.observe('dom:loaded',
         var nodesToRemove = [];
 
         while (node = walker.nextNode()) {
-        	try {
-            	var newNodes = decorateTranslationHints(node);
-            	
-            	if (newNodes == null) {
+            try {
+                var newNodes = decorateTranslationHints(node);
+                
+                if (newNodes == null) {
                     continue;
                 }
-            	var newNode;
+                var newNode;
                 while (newNode = newNodes[0]) {
                     node.parentNode.insertBefore(newNodes[0], node);
                 }
                 nodesToRemove.push(node);
-        	} catch (e) {
-        		console.log(e); 
-        	}
+            } catch (e) {
+                console.log(e); 
+            }
         }
         for (var i = 0; i < nodesToRemove.length; ++i) {
             nodesToRemove[i].parentNode.removeChild(nodesToRemove[i]);
